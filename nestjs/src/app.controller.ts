bda521b947dd20926557482fd9d6a5b79ec678e1
Yogen P <yogendran.poonudurai@setel.com>
2024-07-31 10:02:09 +0800

    included nestjs

---
+++ nestjs/src/app.controller.ts
@@ -0,0 +1,22 @@
+import {Controller, Get, HttpCode, HttpStatus} from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Get('complex')
+  @HttpCode(200)
+  getComplex(): string {
+    let sum = 0;
+    for (let i = 0; i < 1000000; i++) {
+      sum += i;
+    }
+    return `Complex calculation result: ${sum}`;
+  }
+}
